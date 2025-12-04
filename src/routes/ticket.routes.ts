e9a7823ccae60b173a91f5a73a9f38c4db2d097f
diwakarshukla941 <shukladiwakar941@gmail.com>
2025-12-04 23:40:42 +0530

    v1 completed

--- src/routes/ticket.routes.ts
+++ src/routes/ticket.routes.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { generateTicket, getTicketDetails, validateTicket } from "../controllers/ticket.controller";
+import { checkInTicket, checkOutTicket, generateTicket, getTicketDetails, validateTicket } from "../controllers/ticket.controller";
 
 const router = express.Router();
 
 router.post('/generate',generateTicket  );
 router.get('/details/:ticketId', getTicketDetails);
-router.post('/validate',validateTicket)
+router.post('/validate',validateTicket);
+router.post('/checkin', checkInTicket);
+router.post('/checkout', checkOutTicket);
+router.get('/:ticketId', getTicketDetails);
 
 export default router;
